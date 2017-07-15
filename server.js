064f4cf03425eb4bc666d8e08946d489ed57ba84
Ruben Callewaert <5an1ty@users.noreply.github.com>
2017-07-15 18:29:17 +0200

    Initial working version

--- server.js
+++ server.js
@@ -31,41 +31,76 @@ async function retrieveNewBalance(balance) {
   try {
     const newBalance = JSON.parse(JSON.stringify(balance));
     const newTimestamp = unixTimeStamp(new Date().getTime());
-    const transactionsUrl = `/api/transactions?recipientId=${config.address}&AND:fromUnixTime=${newBalance.updateTimestamp}&AND:toUnixTime=${newTimestamp}&AND:limit=1000`;
-    console.log("Retrieving data from " + transactionsUrl);
-    const transactions = await http.get(transactionsUrl);
-    const distributableBalance = transactions.data.transactions.reduce((mem, val) => mem = mem + parseInt(val.amount), 0);
-    const delegates = await Promise.all(config.requiredVotes.map(delegate => http.get(`/api/delegates/get?username=${delegate}`)));
-    const delegateVoters = await Promise.all(delegates.map(async delegate => {
-      const voters = await http.get(`/api/delegates/voters?publicKey=${delegate.data.delegate.publicKey}`);
-      return {delegate: delegate.data.delegate.username, voters: voters.data.accounts};
-    }));
-    let eligableVoters = [];
-    delegateVoters.forEach(delegate => {
-      delegate.voters.forEach(voter => {
-        const found = eligableVoters.filter(eligableVoter => eligableVoter.address == voter.address);
+    const currentBalance = await http.get(`/api/accounts/getBalance?address=${config.address}`);
+    const owedBalance = newBalance.accounts.reduce((mem, val) => mem = mem + parseInt(val.unpaidBalance), 0);
+    const distributableBalance = parseInt(currentBalance.data.balance) - owedBalance;
+    console.log("Distributable balance: " + distributableBalance);
+    if (distributableBalance > 0) {
+      const delegates = await Promise.all(config.requiredVotes.map(delegate => http.get(`/api/delegates/get?username=${delegate}`)));
+      const blacklist = delegates.map(delegate => delegate.data.delegate.address);
+      blacklist.push(config.address);
+      const delegateVoters = await Promise.all(delegates.map(async delegate => {
+        const voters = await http.get(`/api/delegates/voters?publicKey=${delegate.data.delegate.publicKey}`);
+        return {delegate: delegate.data.delegate.username, voters: voters.data.accounts};
+      }));
+      let eligableVoters = [];
+      delegateVoters.forEach(delegate => {
+        delegate.voters.forEach(voter => {
+          const found = eligableVoters.filter(eligableVoter => eligableVoter.address == voter.address);
+          if (found.length === 0) {
+            eligableVoters.push({voter, votes: 1});
+          } else {
+            found[0].votes = found[0].votes + 1;
+          }
+        });
+      });
+      eligableVoters = eligableVoters.filter(eligableVoter => eligableVoter.votes == config.requiredVotes.length && blacklist.filter(delegate => delegate == eligableVoter.voter.address).length === 0);
+      const totalWeight = eligableVoters.reduce((mem, val) => mem = mem + parseInt(val.voter.balance), 0);
+      eligableVoters.forEach(eligableVoter => {
+        const eligibleBalance = math.floor(math.eval(`${distributableBalance} * (${parseInt(eligableVoter.voter.balance)} / ${totalWeight})`));
+        const found = newBalance.accounts.filter(account => account.address == eligableVoter.voter.address);
         if (found.length === 0) {
-          eligableVoters.push({voter, votes: 1});
+          newBalance.accounts.push({address: eligableVoter.voter.address, paidBalance: 0, unpaidBalance: eligibleBalance})
         } else {
-          found[0].votes = found[0].votes + 1;
+          found[0].unpaidBalance = parseInt(found[0].unpaidBalance) + parseInt(eligibleBalance);
         }
       });
-    });
-    eligableVoters = eligableVoters.filter(eligableVoter => eligableVoter.votes == config.requiredVotes.length && delegates.filter(delegate => delegate.data.delegate.address == eligableVoter.voter.address).length === 0);
-    const totalWeight = eligableVoters.reduce((mem, val) => mem = mem + parseInt(val.voter.balance), 0);
-    eligableVoters.forEach(eligableVoter => {
-      const eligibleBalance = math.floor(math.eval(`${distributableBalance} * (${parseInt(eligableVoter.voter.balance)} / ${totalWeight})`));
-      const found = newBalance.accounts.filter(account => account.address == eligableVoter.voter.address);
-      if (found.length === 0) {
-        newBalance.accounts.push({address: eligableVoter.voter.address, paidBalance: 0, unpaidBalance: eligibleBalance})
-      } else {
-        found[0].unpaidBalance = found[0].unpaidBalance + parseInt(eligibleBalance);
-      }
-    });
-    newBalance.updateTimestamp = newTimestamp;
-    return newBalance;
+      newBalance.updateTimestamp = newTimestamp;
+      return newBalance;
+    }
   } catch(e) {
     console.log(e);
+    return balance;
+  }
+}
+
+async function payout(account) {
+  try {
+    if (account.unpaidBalance > LSKToDust(0.2)) {
+      const payoutAmount = account.unpaidBalance - getTransactionFee();
+      const transaction = lisk.transaction.createTransaction(account.address, payoutAmount, config.secret1, config.secret2);
+      const payload = {"transaction": transaction};
+      const netHash = await http.get("/api/blocks/getNethash");
+      const httpConfig = {
+        headers: {
+          "version": "0.8.0",
+          "port": 1,
+          "nethash": netHash.data.nethash
+        }
+      }
+      const paymentRes = await http.post("/peer/transactions/", payload, httpConfig);
+      if (paymentRes.data.success) {
+        account.paidBalance = account.unpaidBalance;
+        account.unpaidBalance = 0;
+      } else {
+        console.log(paymentRes.data);
+      }
+    }
+    return account;
+  } catch(e) {
+    console.log("Payment failed:");
+    console.log(e);
+    return account;
   }
 }
 
@@ -81,6 +116,11 @@ async function app() {
       const newBalance = await retrieveNewBalance(balance);
       console.log(`New owed balance: ${JSON.stringify(newBalance)}`);
       if (newBalance) {
+        const newAccounts = await Promise.all(newBalance.accounts.map(account => payout(account)));
+        newBalance.accounts = newAccounts;
+        console.log(`New processed balance: ${JSON.stringify(newBalance)}`);
+        console.log("Paid : " + newBalance.accounts.reduce((mem, val) => mem = mem + val.paidBalance, 0));
+        console.log("Unpaid : " + newBalance.accounts.reduce((mem, val) => mem = mem + val.unpaidBalance, 0));
         jsonfile.writeFile(balanceFile, newBalance, (err) => {
           if (err) {
             throw err;
